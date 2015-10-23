c589da9ab07bc01af63c36111b9468c085c1d122
Briana O'Hern <brianaohern@gmail.com>
2015-10-22 19:29:10 -0700

    Bri added scrolling background

--- Hail Pizza/Assets/Scripts/BackgroundScript.js
+++ Hail Pizza/Assets/Scripts/BackgroundScript.js
@@ -1,9 +1,15 @@
 ﻿#pragma strict
 
-function Start () {
+var scrollSpeed : float;
+var tileSize : float;
+private var startPosition : Vector3;
 
+function Start () {
+	startPosition = GetComponent(Transform).position;
 }
 
 function Update () {
-
+	var newPosition : float;
+	newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+	GetComponent(Transform).position = startPosition + Vector3(-1,0,0) * newPosition;
 }
