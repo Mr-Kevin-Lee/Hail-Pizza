ef0560c0664874fbeadfded0a71f982ea96d65a7
Mr-Kevin-Lee <stickems94@gmail.com>
2015-10-27 17:45:58 -0700

    gitignore

--- Hail Pizza/Assets/Scripts/PizzaScript.js
+++ Hail Pizza/Assets/Scripts/PizzaScript.js
@@ -1,6 +1,9 @@
 ﻿//var rb: Rigidbody;
 var speed = 10;
+<<<<<<< HEAD
 var health = 4;
+=======
+>>>>>>> origin
 
 function Start() {
 
@@ -10,9 +13,16 @@ function Start() {
 function Update () {
 	GetComponent.<Rigidbody2D>().velocity.y = Input.GetAxis("Vertical") * speed;
 	if (GetComponent(Transform).position.y > 4.5) {
+<<<<<<< HEAD
 	    GetComponent(Transform).position.y = 4.5;
 	} 
 	if (GetComponent(Transform).position.y < -4.5) {
 	    GetComponent(Transform).position.y = -4.5;
+=======
+		GetComponent(Transform).position.y = 4.5;
+	} 
+	if (GetComponent(Transform).position.y < -4.5) {
+		GetComponent(Transform).position.y = -4.5;
+>>>>>>> origin
 	}
 }
