4eda45e9dfe20fb86ebf78c44907320d4486a177
Mr-Kevin-Lee <stickems94@gmail.com>
2015-10-27 17:21:31 -0700

    started work on player health

---
+++ Hail Pizza/Assets/Scripts/PizzaScript.js
@@ -0,0 +1,18 @@
+﻿//var rb: Rigidbody;
+var speed = 10;
+var health = 4;
+
+function Start() {
+
+}
+
+
+function Update () {
+	GetComponent.<Rigidbody2D>().velocity.y = Input.GetAxis("Vertical") * speed;
+	if (GetComponent(Transform).position.y > 4.5) {
+	    GetComponent(Transform).position.y = 4.5;
+	} 
+	if (GetComponent(Transform).position.y < -4.5) {
+	    GetComponent(Transform).position.y = -4.5;
+	}
+}
---
+++ Hail Pizza/Assets/Scripts/PlayerStatsScript.js
@@ -0,0 +1,37 @@
+﻿//#pragma strict
+var player : PizzaScript;
+
+function Start () {
+    //var v3Pos = Vector3(0.0, 1.0, 0.0);
+    //transform.position = gui.camera.ViewportToWorldPoint(v3Pos);
+}
+
+// updates player health and changes sprite accordingly.
+function updatePlayerHealth(addHealth){
+    player = GameObject.Find("PlainPizza").GetComponent(PizzaScript);
+
+    // update health
+    if (addHealth)
+        player.health++;
+    else
+        player.health--;
+
+    // take action based on new health
+    if (player.health > 0) {
+        // player is alive   
+        //GameObject.Find("PlainPizza").GetComponent(SpriteRenderer).sprite = 
+        print("Player Health: " + player.health);
+    }
+    else {
+        // player is dead
+        // gameOver()
+    }
+}
+
+function gameOver() {
+
+}
+
+function Update () {
+    updatePlayerHealth(true);
+}
