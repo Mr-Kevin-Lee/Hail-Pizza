0e25c684d726532ed40ee3a8e68626fabba751a3
Briana O'Hern <brianaohern@gmail.com>
2015-11-08 18:01:40 -0800

    More spawning stuff

--- Hail Pizza/Assets/Scripts/SpawnScript.js
+++ Hail Pizza/Assets/Scripts/SpawnScript.js
@@ -1,19 +1,24 @@
 ﻿#pragma strict
 
-var enemy : GameObject;
-var spawnTime : float = 2;
+var spawnItem : GameObject;
+var spawnTimeMin : float = 1.0;
+var spawnTimeMax : float = 4.0;
 
 function Start () {
-	InvokeRepeating("addEnemy", spawnTime, spawnTime);
+	while (true) {
+		yield WaitForSeconds (Random.Range(spawnTimeMin, spawnTimeMax));
+		addObject();
+	}
+//	InvokeRepeating("addEnemy", spawnTime, spawnTime);
 }
 
-function addEnemy () {
+function addObject () {
 	var y1 = GetComponent(Transform).position.y - GetComponent(Renderer).bounds.size.y/2;
 	var y2 = GetComponent(Transform).position.y + GetComponent(Renderer).bounds.size.y/2;
 	
 	var spawnPoint = new Vector2(GetComponent(Transform).position.x, Random.Range(y1, y2));
 	
-	Instantiate(enemy, spawnPoint, Quaternion.identity);
+	Instantiate(spawnItem, spawnPoint, Quaternion.identity);
 }
 
 // See image below
