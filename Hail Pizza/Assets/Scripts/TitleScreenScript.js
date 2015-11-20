9d2008613dc77d00c32e1ec73e2bc395a535372d
Briana O'Hern <brianaohern@gmail.com>
2015-11-19 18:54:15 -0800

    Title screen and audio

---
+++ Hail Pizza/Assets/Scripts/TitleScreenScript.js
@@ -0,0 +1,30 @@
+﻿//#pragma strict
+
+private var displayLabel = false;
+var instructionText : Text;
+
+function Update () {
+    if (displayLabel)
+        instructionText.text = "PRESS SPACE TO START";
+    else
+        instructionText.text = "";
+
+    if(Input.GetKeyDown("space"))
+    {
+        Application.LoadLevel("Level2");
+    }
+}
+ 
+function Start() {
+    FlashLabel();
+}
+ 
+function FlashLabel() {  
+    while (1) {
+        displayLabel = true;
+        yield WaitForSeconds(.5);
+        displayLabel = false;
+        yield WaitForSeconds(.5); 
+    }
+ 
+}
